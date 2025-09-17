2eaf74a54d41629f3627fa81e7db9cd9c9797f3e
Eda Yuşan <92877794+eda-yusan@users.noreply.github.com>
2025-09-18 01:11:15 +0300

    Update Repository.java

--- src/main/java/com/project/github/model/Repository.java
+++ src/main/java/com/project/github/model/Repository.java
@@ -19,7 +19,7 @@
 //
 //    private String name;
 //
-//    @JsonProperty("stargazers_count")  //Bu anotasyonlar, gelen JSON verisinin alan adlarını Java alanlarına map eder (eşler)  stargazers_count=stargazersCount
+//    @JsonProperty("stargazers_count")  
 //    private int stargazersCount;
 //
 //    @JsonProperty("watchers_count")
@@ -33,38 +33,13 @@
 //    private String licenseName;
 //
 //    @JsonProperty("license")
-//    private void unpackLicense(Map<String, Object> license) {  //	Bu nested JSON içinden name alanını çekip licenseName alanına set eder.
+//    private void unpackLicense(Map<String, Object> license) {  
 //        if (license != null && license.get("name") != null) {
 //            this.licenseName = license.get("name").toString();
 //        }
-//        //GitHub API’sindeki license alanı şöyle gelir:
-////        "license": {
-////            "key": "mit",
-////            "name": "MIT License",
-////             ...
-////        }
-//        //	Bu nested JSON içinden name alanını çekip licenseName alanına set eder.
 //    }
 //
 //}
-//
-////Özetle:
-//
-////Bu sınıf şunları yapar:
-////        •	GitHub API’den çekilen repository verilerini karşılar.
-////	      •	JSON ile gelen verileri doğru alanlara map eder.
-////        •	Veritabanında saklanabilir hale getirir (@Entity).
-////        •	Nested yapıdaki license alanını özel olarak işler.
-//
-//
-////Alan (fields)
-////İd: GitHub repository’nin benzersiz kimliği. (Primary key)
-////name: Repository’nin adı
-////stargazersCount: Repository’nin aldığı yıldız (star) sayısı
-////watchersCount: Takip eden sayısı (yeni sistemde star sayısıyla aynı olabilir)
-////language: Yazılım dili (örneğin: Java, Python)
-////openIssuesCount: Açık (çözülmemiş) issue sayısı
-////licenseName: Lisans adı (MIT, Apache 2.0 vs.)
 
 package com.project.github.model;
 
