830bda850de8475aa1db42a1c8efb8760a8101d2
Eda Yuşan <92877794+eda-yusan@users.noreply.github.com>
2025-09-18 01:12:33 +0300

    Update GitHubAnalyzerApplication.java

--- src/main/java/com/project/github/GitHubAnalyzerApplication.java
+++ src/main/java/com/project/github/GitHubAnalyzerApplication.java
@@ -10,21 +10,8 @@ public class GitHubAnalyzerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitHubAnalyzerApplication.class, args);
-//        Java uygulamasının başlatıldığı yerdir.
-//	•	SpringApplication.run(...) çağrısı:
-//	•	Spring context’i başlatır.
-//	•	Otomatik konfigürasyonları yapar.
-//	•	Tüm bean’leri oluşturur.
-//	•	Uygulamayı ayakta tutan bir embedded server (Tomcat) başlatır.
+
     }
 }
 
-//@SpringBootApplication
-
-//Bu anotasyon 3 önemli anotasyonu birleştirir:
-//        1.	@Configuration: Bu sınıf bir Spring config (yapılandırma) sınıfıdır.
-//        2.	@EnableAutoConfiguration: Spring Boot’un otomatik konfigürasyon özelliklerini aktif eder.
-//        3.	@ComponentScan: Uygulamanın bulunduğu paketteki tüm bileşenleri (@Service, @Controller, @Repository vb.) otomatik olarak tarar.
-//
-//Bu sayede uygulama “sıfır konfigürasyonla” çalışabilir hale gelir.
 
